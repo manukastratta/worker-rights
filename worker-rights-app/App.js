ca3dd5480af898638c31ab1983f99e1dbde7486a
Manuka Stratta <manukavalentina@hotmail.com>
2020-03-28 23:37:41 -0700

    Fixed old dependency issues by recreating expo project

--- worker-rights-app/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import RightsScreen from './screens/RightsScreen';
-
-export default function App() {
-  return (
-    <RightsScreen />
-  );
-}
